cbb2a3a00a61ba501aedf0cf0a3b81abf313d3e0
jimmyale3102 <jimmyale3102@gmail.com>
2021-11-11 15:31:57 -0500

    Get retrofit

--- app/src/main/java/dev/alejo/fletxtest/app/MainActivity.kt
+++ app/src/main/java/dev/alejo/fletxtest/app/MainActivity.kt
@@ -2,12 +2,21 @@ package dev.alejo.fletxtest.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun getRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://st.fletx.co:3000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 
 }
